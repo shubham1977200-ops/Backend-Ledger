ebc23312e360e55decd6281c900fa114b9316599
shubham1977200-ops <shubham1977200@gmail.com>
2026-09-12 17:18:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ const transactionRoutes = require('./routes/transaction.routes')
 /**
  * - Use Routes
  */
-
+app.get("/",(req,res)=>{
+    res.send("Ledger Service is up and running")
+})
 app.use('/api/auth',authRouter)
 
 app.use('/api/accounts',accountRouter)
